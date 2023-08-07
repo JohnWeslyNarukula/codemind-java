c74f1c16f01f57a8d3fcddbc17b3fbe43265142b
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-08-07 14:28:22 +0530

    import java.util.Scanner; public class john {     public static void main(String[] args)     {         Scanner wesly=new Scanner(System.in);         double n,m,ans;         n=wesly.nextDouble();         m=wesly.nextDouble();         ans=n*m;         System.out.printf("%.2f",ans);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class john
+{
+    public static void main(String[] args)
+    {
+        Scanner wesly=new Scanner(System.in);
+        double n,m,ans;
+        n=wesly.nextDouble();
+        m=wesly.nextDouble();
+        ans=n*m;
+        System.out.printf("%.2f",ans);
+    }
+}
