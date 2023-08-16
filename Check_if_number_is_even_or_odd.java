c4d48934f87914a5f1b9ad85e11aaec1b7a55600
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-08-16 06:36:10 +0530

    import java.util.Scanner; public class john {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n;         n = sc.nextInt();         if(n%2==0)         {             System.out.println("Even");         }         else         {             System.out.println("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class john
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
