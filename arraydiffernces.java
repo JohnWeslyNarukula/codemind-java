7df0c0f6b78fdffae32afa07cf5b3f34917fb5df
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-09-03 21:32:22 +0530

    Add files via upload

---
+++ arraydiffernces.java
@@ -0,0 +1,9 @@
+public class arraydiffernces {
+    public static void main(String[] args)
+    {
+        int[] a,b,c;
+        int d[],e,f;
+        //only d is integer array e and f are normal integers
+
+    }
+}
