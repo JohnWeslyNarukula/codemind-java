ec8ed1ec9b232fc05e10d3c31e22e098db73dfd5
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-09-11 17:21:43 +0530

    import java.util.Scanner; public class john {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         String h = "Hello Technicalhub";         String s = sc.nextLine();         System.out.println(h);         System.out.println(s);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class john
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String h = "Hello Technicalhub";
+        String s = sc.nextLine();
+        System.out.println(h);
+        System.out.println(s);
+    }
+}
