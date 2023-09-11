7747e9f2a06afcfc6b2e73fcf07868dfe5936e9b
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-09-11 17:29:16 +0530

    import java.util.Scanner; public class john {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         String s = sc.nextLine();         int sum = 0;         for(int i = 0 ; i < s.length() ; i++)         {             if(Character.isDigit(s.charAt(i)))             sum=sum+Character.getNumericValue(s.charAt(i));         }         System.out.println(sum);     } }

---
+++ _Sum_of_all_numbers_present_in_the_String.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class john
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int sum = 0;
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            if(Character.isDigit(s.charAt(i))) 
+            sum=sum+Character.getNumericValue(s.charAt(i));
+        }
+        System.out.println(sum);
+    }
+}
