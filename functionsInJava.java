9e852cda3ccd88829956c8dd29a06bde1a5f38a2
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-09-14 22:33:26 +0530

    Add files via upload

---
+++ functionsInJava.java
@@ -0,0 +1,10 @@
+public class functionsInJava {
+    static void sayHello() {
+    System.out.println("Hello EveryOne");
+    }
+    public static void main(String[] args)
+    {
+        functionsInJava f = new functionsInJava();
+        f.sayHello();
+    }
+}
