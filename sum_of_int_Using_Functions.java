59aae46c5d5869f86e9d0555ecae4225d944bc12
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-09-23 22:45:36 +0530

    Add files via upload

---
+++ sum_of_int_Using_Functions.java
@@ -0,0 +1,10 @@
+public class sum_of_int_Using_Functions {
+    static int addnum(int n1,int n2,int n3,int n4,int n5) {
+        return n1+n2+n3+n4+n5;
+        }
+        public static void main(String[] args)
+        {
+          int result=addnum(10,20,30,40,50);
+          System.out.println(result);
+        }
+    }
