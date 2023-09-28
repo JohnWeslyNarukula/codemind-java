ba8e95e49a5579428d08ee00e95db906fed813f9
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-09-28 22:36:37 +0530

    Add files via upload

---
+++ brstm.java
@@ -0,0 +1,16 @@
+public class brstm
+{
+    public static void main(String[] args)
+    {
+        int i = 1;
+        while(true)
+        {
+            System.out.println(i +" ");
+            if(i == 10)
+            {
+                break;
+            }
+            i++;
+        }
+    }
+}
