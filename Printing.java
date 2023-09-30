7d4645aa1fdf2146ec2254ae1a4ee04d4dc3cd6c
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-09-30 20:06:59 +0530

    Add files via upload

---
+++ Printing.java
@@ -0,0 +1,17 @@
+public class Printing {
+    public static void main(String[] args)
+    {
+        int a = 10;
+        double b = 12.2;
+        String s = "Hello";
+        float x = 12.7f;
+        byte d = 3;
+        char c = 'a';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(x);
+        System.out.println(d);
+        System.out.println(c);
+    }
+}
