49a418b26707873940bd125f2fb3d4276643e056
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-10-03 20:46:14 +0530

    Add files via upload

---
+++ PhNo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class PhNo {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s= sc.nextLine();
+        int c=s.length();
+        if(c==10){
+            System.out.println("Valid Number");
+        }
+        }
+    }
