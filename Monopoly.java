4d0fb9e9a1ec4566599486ee5f69803ce361cbd1
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-11-02 21:41:05 +0530

    Add files via upload

--- Monopoly.java
+++ Monopoly.java
@@ -1,24 +1,2 @@
-import java.util.Scanner;
-public class john
-{
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner (System.in);
-        int a,b,c,m,i;
-        m = sc.nextInt();
-        for(i=0;i<m;i++)
-        {
-            a = sc.nextInt();
-            b = sc.nextInt();
-            c = sc.nextInt();
-            if(a+b<c || b+c<a || c+a<b)
-            {
-                System.out.println("YES");
-            }
-            else
-            {
-                System.out.println("NO");
-            }
-        }
-    }
-}
+n=int(input())
+for i in range(n)
