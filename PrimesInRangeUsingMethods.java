57fdc842b6cad4e76859b1c4511dd21a917d99b3
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-11-05 21:11:30 +0530

    Add files via upload

---
+++ PrimesInRangeUsingMethods.java
@@ -0,0 +1,19 @@
+public class PrimesInRangeUsingMethods 
+{
+    public static boolean isprime(int n){
+        if(n<=1){
+            return false;
+        }
+        for(int i=2;i<=n;i++){
+            
+        }
+    }
+    int start=1;
+    int end=100;
+    for(int j=start;i<=end;i++)
+    {
+        if(isprime(j)){
+            System.out.println(j);
+        }
+    }
+}
