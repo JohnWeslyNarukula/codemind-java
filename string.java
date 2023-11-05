f30f520c7e327248ee5fafa6906e57f0f8039bae
22a91a61h2 <119839139+22a91a61h2@users.noreply.github.com>
2023-11-05 21:11:09 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class string{
+    public static void main(String[] args){
+        
+        String s;
+        Scanner hi = new Scanner(System.in);
+        s = hi.nextLine();
+        System.out.println(s);
+    }
+}
